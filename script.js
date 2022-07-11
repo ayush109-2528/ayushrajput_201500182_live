2628aae03a9d9ca1404f6179dbe62683782383fd
ayush <ayushrajput109@gmail.com>
2022-07-11 20:13:29 +0530

    my template

---
+++ script.js
@@ -0,0 +1,11 @@
+var today = new Date();
+  var day = today.getDay();
+  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  var date = today.getDate()+'-'+ (today.getMonth()+1)+'-'+today.getFullYear();
+  document.getElementById("date").innerHTML = date;
+ var name = 'Ayush Rajput';
+ document.getElementById('name').innerHTML=name;
+ var roll = '201500182';
+ document.getElementById('roll').innerHTML=roll;
+ var university = 'GLA University';
+ document.getElementById('university').innerHTML=university;
